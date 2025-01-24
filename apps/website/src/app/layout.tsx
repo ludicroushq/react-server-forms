dca3138ae36b002681a0e99cd3db695bdcde81fd
Manthan Mallikarjun <nahtnam@gmail.com>
2025-01-24 01:06:35 -0800

    fix build

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased`}>
-        <Providers>{children}</Providers>
+        <Suspense>
+          <Providers>{children}</Providers>
+        </Suspense>
       </body>
     </html>
   );
